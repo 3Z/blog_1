a51c9d4ec5ea8f42316f87773448189c61f84ca9
cheng <njzhaoguobin@126.com>
2011-11-20 11:37:03 +0800

    10. add tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ActiveRecord::Base
   validates :title, :presence => true, :length => { :minimum => 5 }
   
   has_many :comments, :dependent => :destroy
+  has_many :tags
+
+  accepts_nested_attributes_for :tags, :allow_destroy => :true,
+    :reject_if => proc { |attrs| attrs.all? { |k,v| v.blank? } }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111117151629) do
+ActiveRecord::Schema.define(:version => 20111120030027) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -30,5 +30,14 @@ ActiveRecord::Schema.define(:version => 20111117151629) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tags", ["post_id"], :name => "index_tags_on_post_id"
 
 end
